543dcbeb12982835a8062045662dd4b52602db33
onileola <onileola14@gmail.com>
2026-08-21 22:45:43 +0100

    modified:   dist/index.js 	modified:   index.ts

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,7 @@
 "use strict";
 const userName = "John Doe";
+const age = 30;
+const isAdmin = true;
 console.log(userName);
+console.log(age);
+console.log(isAdmin);
--- index.ts
+++ index.ts
@@ -1,2 +1,6 @@
 const userName: string = "John Doe";
+const age : number = 30;
+const isAdmin: boolean = false;
 console.log(userName);
+console.log(age);
+console.log(isAdmin);
